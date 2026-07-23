4cdfa8e891dca7a3310ca6540f04bacdc2ed498f
Lucas Olopes <olopes.lucas567@gmail.com>
2026-07-23 20:26:33 -0300

    feat(web): fontes self-hosted via @fontsource-variable (remove CDN e geist)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// eslint-disable-next-line import/no-unassigned-import
+import '@fontsource-variable/space-grotesk'
+// eslint-disable-next-line import/no-unassigned-import
+import '@fontsource-variable/hanken-grotesk'
+// eslint-disable-next-line import/no-unassigned-import
+import '@fontsource-variable/jetbrains-mono'
 import './index.css'
 import { App } from './app/App.tsx'
 
