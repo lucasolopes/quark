4967eef506ce81840c4f1ef861befd47e9353733
Lucas Olopes <olopes.lucas567@gmail.com>
2026-07-23 21:24:16 -0300

    fix(web): input v2 comporta o texto (h-10) e ultima linha da tabela sem hairline

--- web/src/components/ui/table.tsx
+++ web/src/components/ui/table.tsx
@@ -31,7 +31,7 @@ function TableBody({ className, ...props }: React.ComponentProps<"tbody">) {
   return (
     <tbody
       data-slot="table-body"
-      className={cn("[&_tr:last-child]:border-0", className)}
+      className={cn("[&_tr:last-child]:border-0 [&_tr:last-child_td]:border-b-0", className)}
       {...props}
     />
   )
